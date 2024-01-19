05e842e93b709d220ed2603d9f480c8930f52ad3
k0dm <fighttournament2@gmail.com>
2024-01-19 15:14:36 +0200

    change status bar color

--- app/src/main/java/com/github/k0dm/unscramble/WordWithScore.kt
+++ app/src/main/java/com/github/k0dm/unscramble/WordWithScore.kt
@@ -3,6 +3,7 @@ package com.github.k0dm.unscramble
 data class WordWithScore(private val word: String, private val score: Int) : ShuffleWordAndScore {
 
     override fun shuffledWord() = word.reversed()
+
     override fun checkIsTheSame(word: String) = this.word == word
 
     override fun score(attempt: Int): Int {
